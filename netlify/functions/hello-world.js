d6472744ebec8f133af62003e9ecc7367243b4a7
Treasure Porth <treasure@scrimba.com>
2022-08-25 12:14:27 -0700

    Add CORS headers

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,8 +1,14 @@
 exports.handler = async function(){
+  const CORS_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers':
+        'Origin, X-Requested-With, Content-Type, Accept',
+    }
   return {
     statusCode: 200, 
     body: JSON.stringify({
       message: 'Hello World'
-    })
+    }),
+    headers: CORS_HEADERS
   }
 }
